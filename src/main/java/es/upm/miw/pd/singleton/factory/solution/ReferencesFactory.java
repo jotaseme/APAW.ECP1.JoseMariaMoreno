fec93116ccddf381b5505e6458ef2bb61342452e
Jose Maria <jotaememoreno@gmail.com>
2016-10-07 18:48:52 +0200

    ReferencesFactory singleton tested and working

--- src/main/java/es/upm/miw/pd/singleton/factory/solution/ReferencesFactory.java
+++ src/main/java/es/upm/miw/pd/singleton/factory/solution/ReferencesFactory.java
@@ -4,37 +4,37 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ReferencesFactory {
-    private Map<String, Integer> references;
-    
-    private static ReferencesFactory referencesFactory;
+	private Map<String, Integer> references;
 
-    private int reference;
+	private static ReferencesFactory referencesFactory;
 
-    private ReferencesFactory() {
-        this.references = new HashMap<>();
-        this.reference = 0;
-        referencesFactory = null;
-    }
+	private int reference;
 
-    public int getReference(String key) {
-        Integer result = this.references.get(key);
-        if (result == null) {
-            this.references.put(key, this.reference);
-            result = this.reference;
-            reference++;
-        }
-        return result;
-    }
+	private ReferencesFactory() {
+		this.references = new HashMap<>();
+		this.reference = 0;
+		referencesFactory = null;
+	}
 
-    public void removeReference(String key) {
-        this.references.remove(key);
-    }
-    
-    public static ReferencesFactory getReferencesFactory(){
-    	if(ReferencesFactory.referencesFactory==null){
-    		ReferencesFactory.referencesFactory = new ReferencesFactory();
-    	}
-    	return ReferencesFactory.referencesFactory;
-    }
+	public int getReference(String key) {
+		Integer result = this.references.get(key);
+		if (result == null) {
+			this.references.put(key, this.reference);
+			result = this.reference;
+			reference++;
+		}
+		return result;
+	}
+
+	public void removeReference(String key) {
+		this.references.remove(key);
+	}
+
+	public static ReferencesFactory getReferencesFactory() {
+		if (ReferencesFactory.referencesFactory == null) {
+			ReferencesFactory.referencesFactory = new ReferencesFactory();
+		}
+		return ReferencesFactory.referencesFactory;
+	}
 
 }
