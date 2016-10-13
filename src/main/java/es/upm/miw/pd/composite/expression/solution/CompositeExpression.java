4a7ba1cfa163dcb75488fa4414848e3c9bb2b969
Jose Maria <jotaememoreno@gmail.com>
2016-10-13 19:02:13 +0200

    Creation of composite solution package and clases. Test doesnt work yet.

---
+++ src/main/java/es/upm/miw/pd/composite/expression/solution/CompositeExpression.java
@@ -0,0 +1,7 @@
+package es.upm.miw.pd.composite.expression.solution;
+
+public abstract class CompositeExpression extends Expression {
+
+	public abstract int operar();
+
+}
---
+++ src/main/java/es/upm/miw/pd/composite/expression/solution/Multiplicacion.java
@@ -0,0 +1,11 @@
+package es.upm.miw.pd.composite.expression.solution;
+
+public class Multiplicacion extends Expression {
+
+	@Override
+	public int operar() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
