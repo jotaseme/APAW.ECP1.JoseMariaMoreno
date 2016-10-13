e021b6d212ce29ad9424682e6b9f0584c83a1992
Jose Maria <jotaememoreno@gmail.com>
2016-10-13 19:17:05 +0200

    Number class finished

--- src/main/java/es/upm/miw/pd/composite/expression/solution/Number.java
+++ src/main/java/es/upm/miw/pd/composite/expression/solution/Number.java
@@ -2,10 +2,19 @@ package es.upm.miw.pd.composite.expression.solution;
 
 public class Number extends Expression {
 
+	private int value;
+
+	public Number(int value) {
+		this.value = value;
+	}
+
 	@Override
 	public int operar() {
-		// TODO Auto-generated method stub
-		return 0;
+		return value;
+	}
+
+	public String toString() {
+		return String.valueOf(value);
 	}
 
 }
