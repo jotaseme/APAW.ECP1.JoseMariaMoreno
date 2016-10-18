d3b6f1a374bb09b68c0c0af87f23dd409058adff
Jose Maria <jotaememoreno@gmail.com>
2016-10-18 20:49:53 +0200

    Refactoring of conexion class with polymorphysms

--- src/main/java/es/upm/miw/pd/state/connection/solution/Conexion.java
+++ src/main/java/es/upm/miw/pd/state/connection/solution/Conexion.java
@@ -1,16 +1,55 @@
 package es.upm.miw.pd.state.connection.solution;
 
 import es.upm.miw.pd.state.conection.solution.LinkMock;
+import es.upm.miw.pd.state.connection.solution.Estado;
 
 public class Conexion {
 	private Estado estado;
+	private LinkMock link;
 
 	public Conexion(LinkMock link) {
+		assert link != null;
+		this.link = link;
 		this.setEstado(new Parado());
 	}
 
 	protected void setEstado(Estado estado) {
 		this.estado = estado;
 	}
-
+	
+	public Estado getEstado() {
+        return this.estado;
+    }
+	
+	public LinkMock getLink() {
+		return link;
+	}
+	
+	public void abrir(){
+		estado.abrir(this);
+	}
+    
+    public void cerrar(){
+    	estado.cerrar(this);
+    }
+    
+    public void enviar(String msg){
+    	estado.enviar(msg, this);
+    }
+    
+    public void recibir(int respuesta){
+    	estado.recibir(respuesta, this);
+    }
+    
+    public void iniciar(){
+    	estado.iniciar(this);
+    }
+    
+    public void parar(){
+    	estado.parar(this);
+    }
+    
+    public String toString(){
+		return null;
+    }
 }
