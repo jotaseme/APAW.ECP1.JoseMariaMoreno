7de807aad071a9c75a8b29b8bfa4cb7d60a72143
Jose Maria <jotaememoreno@gmail.com>
2016-10-18 20:57:35 +0200

    Implementation of methods in class Parado

--- src/main/java/es/upm/miw/pd/state/connection/solution/Parado.java
+++ src/main/java/es/upm/miw/pd/state/connection/solution/Parado.java
@@ -4,44 +4,40 @@ public class Parado extends Estado{
 
 	@Override
 	public void abrir(Conexion conexion) {
-		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Acción no permitida... ");
 		
 	}
 
 	@Override
 	public void cerrar(Conexion conexion) {
-		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Acción no permitida... ");
 		
 	}
 
 	@Override
 	public void enviar(String msg, Conexion conexion) {
-		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Acción no permitida... ");
 		
 	}
 
 	@Override
 	public void recibir(int respuesta, Conexion conexion) {
-		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Acción no permitida... ");
 		
 	}
 
 	@Override
 	public void iniciar(Conexion conexion) {
-		// TODO Auto-generated method stub
-		
+		conexion.setEstado(new Preparado());		
 	}
 
 	@Override
-	public void parar(Conexion conexion) {
-		// TODO Auto-generated method stub
-		
+	public void parar(Conexion conexion) {		
 	}
 
 	@Override
 	public String toString() {
-		// TODO Auto-generated method stub
-		return null;
+		return "Parado";
 	}
 
 }
