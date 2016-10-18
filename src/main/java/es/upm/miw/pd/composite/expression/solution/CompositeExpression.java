af2e3be5057fb5b31ebe4733ba8465eb874068ee
Jose Maria <jotaememoreno@gmail.com>
2016-10-18 19:31:58 +0200

    Composite created with constructor, getters and setters

--- src/main/java/es/upm/miw/pd/composite/expression/solution/CompositeExpression.java
+++ src/main/java/es/upm/miw/pd/composite/expression/solution/CompositeExpression.java
@@ -1,7 +1,32 @@
 package es.upm.miw.pd.composite.expression.solution;
 
 public abstract class CompositeExpression extends Expression {
-
+	
+	
+	private Expression exp1;
+	private Expression exp2;
+	
+	public CompositeExpression(Expression exp1,Expression exp2){
+        this.exp1=exp1;
+        this.exp2=exp2;
+    }
+    
+    public Expression getExpression1(){
+        return this.exp1;
+    }
+    
+    public Expression getExpresion2(){
+        return this.exp2;
+    }
+    
+    public void setExpresion1(Expression exp1){
+        this.exp1 = exp1;
+    }
+        
+    public void setExpresion2(Expression exp2){
+        this.exp2 = exp2;
+    }
+	
 	public abstract int operar();
 
 }
