43cfec8b9c5460d3de04a7caf50038db3d3d88f3
Jose Maria <jotaememoreno@gmail.com>
2016-10-20 23:15:01 +0200

    Refactoring - Added visibility to Comando interface methods

--- src/main/java/es/upm/miw/pd/command/calculator/solution/Comando.java
+++ src/main/java/es/upm/miw/pd/command/calculator/solution/Comando.java
@@ -1,8 +1,7 @@
 package es.upm.miw.pd.command.calculator.solution;
 
 
-public interface Comando {
-   
-	void execute();
-	String name();
+public interface Comando {  
+	public void execute();
+	public String name();
 }
