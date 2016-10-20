d3a1d461646681d023dd2e6fa5b3efb8acfb958d
Jose Maria <jotaememoreno@gmail.com>
2016-10-20 20:46:28 +0200

    Composite pattern applied. Flyweight pattern is next.

--- src/main/java/es/upm/miw/pd/text/solution/Texto.java
+++ src/main/java/es/upm/miw/pd/text/solution/Texto.java
@@ -3,8 +3,6 @@ package es.upm.miw.pd.text.solution;
 public abstract class Texto {
 	
 	public abstract void add(Texto componente);
-    public abstract String dibujar(boolean mayusculas);+
+    public abstract String dibujar(boolean mayusculas);
     public abstract boolean isCaracter();
-   
-
 }
